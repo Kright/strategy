0901500a4f46a970f96c175c76dc347be4e92162
AVova <alexenkovova@yandex.ru>
2014-03-03 17:44:38 +0400

    Randomiser

--- src/game/main/utils/Randomiser.java
+++ src/game/main/utils/Randomiser.java
@@ -6,27 +6,36 @@ import android.os.SystemClock;
  * Created by user on 03.03.14.
  */
 final public class Randomiser {
-    public int rnd ()
+
+    public  float getFloat()
     {
-        previous = state;
-        state = (a*state+b) % m;
-        return state % 32768;
+        float f = (float)rnd() / (float)m;
+        return f;
+    }
+    public int getInt(int n){
+        return rnd() % n;
+    }
+    private int rnd ()
+    {
+        previous = seed;
+        seed = (a*seed+b) % m;
+        return seed ;
     }
     public void back()
     {
-        state = previous;
+        seed = previous;
     }
 
-
-    public void changeParam(int aa,int bb, int mm)
-    {
-        this.a=aa;
-        this.b=bb;
-        this.m=mm;
+    public int getSeed(){
+        return seed;
     }
-    private int state = (int)SystemClock.elapsedRealtime() % 100000;
+    public void setSeed(int x){
+        seed = x;
+    }
 
-    private int previous = state;
+    private int seed = (int)SystemClock.elapsedRealtime() % 100000;
+
+    private int previous = seed;
     private int a=4081;
     private int b=25673;
     private int m=121500;

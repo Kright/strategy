2efd3b25c0f6484313abff88eb5f4da635e5d61e
lgor <forxgames007@gmail.com>
2014-04-05 00:40:14 +0400

    переименование

--- src/game/main/GameThread.java
+++ src/game/main/GameThread.java
@@ -1,10 +1,10 @@
 package game.main;
 
 import android.graphics.Canvas;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.View;
+import game.main.gamelogic.GameSession;
 import game.main.utils.Touch;
 
 import java.util.ArrayList;
@@ -14,55 +14,51 @@ import java.util.List;
  * поток с игрой. Запускается один раз, если приложение сворачивают - приостанавливается.
  * Created by lgor on 02.04.14.
  */
-public abstract class GameThread2 extends Thread implements View.OnTouchListener{
+public class GameThread extends Thread implements View.OnTouchListener{
 
-    private volatile Object monitor;
+    private final Object monitor;
     private volatile boolean reallyWait=false;
     private volatile boolean mustWait=true;
     private volatile SurfaceHolder holder;
+    private final GameSession session;
 
-    protected GameThread2(Object monitor){
+    protected GameThread(Object monitor, GameSession session){
         this.monitor = monitor;
+        this.session = session;
+        session.setCallback(this);
         this.start();
         this.setWaiting();
     }
 
-    /**
-     * main loop there
-     */
-    public abstract void main();
-
-    /**
-     * @param canvas, на котором мы рисуем. Этот метод вызывается, если вызвать repaint()
-     */
-    public abstract void paint(Canvas canvas);
-
     @Override
     public final void run() {
         checkPause();
-        main();
+        session.run();
     }
 
-    protected final void repaint(){
+    public final boolean repaint(){
+        boolean success=false;
         Canvas canvas=null;
         try {                       // получаем объект Canvas и выполняем отрисовку
             canvas = holder.lockCanvas(null);
             if (canvas != null)
                 synchronized (holder) {
-                    paint(canvas);
+                    session.paint(canvas);
+                    success = true;
                 }
         } finally {
             if (canvas != null) {   // отрисовка выполнена. выводим результат на экран
                 holder.unlockCanvasAndPost(canvas);
             }
         }
+        return success;
     }
 
     /**
      * если извне потоку посоветовали остановиться, то он остановится, только когда мы изнутри вызовем эту функцию
      * @return true if thread was paused;
      */
-    protected final boolean checkPause(){
+    public final boolean checkPause(){
         Thread.yield();         //без этого приложение может нас радовать чёрным экраном довольно долгое время
                                 //если в игровом цикле нет ничего, нагружающего телефон, а иначе работает и без yield
         boolean paused=false;
@@ -75,6 +71,9 @@ public abstract class GameThread2 extends Thread implements View.OnTouchListener
                 } catch (InterruptedException e) {
                 }
             }
+        }
+        if (paused){
+            session.resume();
         }
         return paused;
     }
@@ -97,7 +96,6 @@ public abstract class GameThread2 extends Thread implements View.OnTouchListener
     /*
      * обработка нажатий на экран
      */
-
     private final List<Touch> touches = new ArrayList<Touch>();
     private Touch prev;
 

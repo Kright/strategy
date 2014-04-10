192931c2b69d6aa3ea00d76b47ef88df5ac799ae
mickmenn <mickmenn@yandex.ru>
2014-04-10 11:50:02 +0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/game/main/gamelogic/Map.java

--- src/game/main/gamelogic/world/Village.java
+++ src/game/main/gamelogic/world/Village.java
@@ -14,19 +14,26 @@ public class Village extends Settlement {
 
     protected int population; // население
     protected int wealth; // благосостояние
+    protected int wellBeing; // благополучие
+    protected float levelOfTaxes;
+    protected ResourcesCounter rCounter;
+    //protected int growthOfPopulation;
+    //protected int growthOfWealth;
 
     protected ArrayList<Cell> fields;
+
 
 
     public Village(Country country, Cell cell) {
         super(country, cell);
         population = 1;// условно
         wealth = 1;// условно
+        wellBeing=1;
     }
 
     @Override
     public int getTaxes() {
-        return population;
+        return (int)(levelOfTaxes*population);
     }
 
     @Override
@@ -37,7 +44,30 @@ public class Village extends Settlement {
     @Override
     public void nextTurn() {
 
+        produce();
+        changeWellBeing();
+        population+=spawn();
+        wealth+=rCounter.gold;
+        rCounter.clear();
     }
+
+    private int spawn(){
+            return population+wellBeing-getTaxes();
+    }
+
+
+
+    private void produce(){
+        int profitf;
+        for(Cell c:fields){
+            c.produce(rCounter);
+        }
+    }
+
+    private int changeWellBeing(){
+        return wellBeing;
+    }
+
 
     protected static Sprite sprite;
 

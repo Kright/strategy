bdf2ceb96be0249b1b711372c2811deed3014cf4
lgor <forxgames007@gmail.com>
2014-04-27 16:38:51 +0400

    added another activity which uses openGL and does nothing

---
+++ src/game/main/openGL/GLActivity.java
@@ -0,0 +1,40 @@
+package game.main.openGL;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+/**
+ * Ещё одно, альтернативное активити.
+ * Будет использовано для того, чтобы устроить рисование игры через openGL
+ * Created by lgor on 27.04.14.
+ */
+public class GLActivity extends Activity{
+
+    private GLSurfaceView viewGL;
+    private GLRenderer renderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewGL = new GLSurfaceView(this);
+        viewGL.setPreserveEGLContextOnPause(true);
+        viewGL.setEGLContextClientVersion(2);
+        renderer = new GLRenderer();
+        viewGL.setRenderer(renderer);
+        viewGL.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+        setContentView(viewGL);
+    }
+
+    @Override
+    protected void onResume() {
+        viewGL.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        viewGL.onPause();
+        super.onPause();
+    }
+}
---
+++ src/game/main/openGL/GLRenderer.java
@@ -0,0 +1,29 @@
+package game.main.openGL;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Implementation of methods - initialization and drawing, which called in graphics thread.
+ * Created by lgor on 27.04.14.
+ */
+class GLRenderer implements GLSurfaceView.Renderer{
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+        GLES20.glClearColor(0, 0, 0, 1);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
